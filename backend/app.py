1e4b26559ca6bf8f51fa3a6f49f61540baa430f6
gen-yuu <yuki12092002@icloud.com>
2025-02-18 14:37:25 +0900

    backendとの接続を試みて

--- backend/app.py
+++ backend/app.py
@@ -4,15 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # ReactとFlaskが別サーバーならCORSを有効化
 
-@app.route("/api/cart")
-def get_cart():
-    return jsonify({
-        "items": [
-            {"name": "りんご", "price": 200},
-            {"name": "バナナ", "price": 100}
-        ],
-        "total": 300
-    })
+@app.route("/api/item", methods=['GET'])
+def get_item():
+    return jsonify(
+        {"id": 1, "name":"烏龍茶","price":100,"code":"112468","class":"drink","stock_num":2}
+    )
 
 @app.route("/api/card")
 def get_card():
--- frontend/src/features/purchase/App.js
+++ frontend/src/features/purchase/App.js
@@ -1,8 +1,20 @@
 import { useEffect, useState } from 'react';
 import ItemList from './ItemList';
+import axios from "axios";
+
+
 
 function App() {
-  const [items,setItems] = useState([{id:1,price:150}])
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://backend:5000/api/data")
+      .then((response) => {
+        setItems((prevItems) => [...prevItems, response.data]); // 配列に追加
+      })
+      .catch((error) => console.error(error));
+  }, []);
+  
   return (
     <>
       <ItemList items={items}/>
